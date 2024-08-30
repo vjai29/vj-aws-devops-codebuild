ed47ded610a233a4e002960c19cbee8832016ae6
Sarath Kumar <03sarath@gmail.com>
2024-08-30 21:47:26 +0530

    Initial commit

---
+++ your_script.py
@@ -0,0 +1,18 @@
+import requests
+
+def get_random_joke():
+    url = "https://official-joke-api.appspot.com/random_joke"
+    
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        joke = response.json()
+        setup = joke['setup']
+        punchline = joke['punchline']
+        print(f"Joke: {setup}")
+        print(f"Punchline: {punchline}")
+    else:
+        print("Failed to retrieve data")
+
+if __name__ == "__main__":
+    get_random_joke()
